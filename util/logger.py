96f109b2ce4a521c72679e066810cd7cdfb4de0c
hairinwind <dong_yao@hotmail.com>
2019-04-02 21:23:45 -0700

    make it easy to debug

--- util/logger.py
+++ util/logger.py
@@ -1,11 +1,16 @@
 import logging
 import time
 from logging.handlers import TimedRotatingFileHandler
+from util import envUtil
  
 logger = logging.getLogger()
 
 def initLogger(path='log/pyahoo.log'):
-    logger.setLevel(logging.INFO)
+    if envUtil.isDev():
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+        
     # format the log entries
     formatter = logging.Formatter('%(asctime)s %(levelname)s [%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s')
  
