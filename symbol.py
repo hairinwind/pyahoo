33d2748ca581f2eb2a52af36f2fe8efc83235c23
hairinwind <dong_yao@hotmail.com>
2021-08-21 12:27:22 -0700

    read symbols from GIT

--- symbol.py
+++ symbol.py
@@ -1,6 +1,7 @@
 from os import environ
 from util.logger import logger
 import numpy as np
+import requests
 
 symbolDir = 'config/'
 
@@ -11,9 +12,17 @@ def getSymbolFileName():
         return 'symbol.txt'
 
 def readSymbolsFromFile():
-    symbolFileName = symbolDir + getSymbolFileName()
-    symbols = np.loadtxt(symbolFileName, dtype='str')
-    return np.unique(symbols)
+    symbol_file_name = getSymbolFileName()
+    if symbol_file_name.startswith('http'):
+        # read file from remote
+        symbol_text = requests.get(symbol_file_name).content.decode("utf-8")
+        symbols = symbol_text.splitlines()
+        symbols = np.unique(symbols)
+        return symbols
+    else:
+        symbolFileName = symbolDir + symbol_file_name
+        symbols = np.loadtxt(symbolFileName, dtype='str')
+        return np.unique(symbols)
 
 if __name__=="__main__":
     symbols = readSymbolsFromFile()
